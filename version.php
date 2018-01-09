31ac9e6b676d079de61b21ed1f85dc241e5feca5
valery <valery.fremaux@gmail.com>
2018-01-09 12:23:07 +0100

    first fixes

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2017112200;
-$plugin->requires = 2016112900;
+$plugin->requires = 2017050500;
 $plugin->component = 'report_examtraining';
-$plugin->release = '3.2.0 (Build 2017112200)';
+$plugin->release = '3.3.0 (Build 2017112200)';
 $plugin->maturity = MATURITY_BETA;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0002';
+$plugin->codeincrement = '3.3.0002';
